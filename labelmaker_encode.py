cdb66b1f5e743b51617ebafa79cf1e8f39448e7f
Philipp Schmitt <philipp@schmitt.co>
2026-03-01 17:37:43 +0100

    Trim NetBox label width

--- labelmaker_encode.py
+++ labelmaker_encode.py
@@ -2,6 +2,22 @@ import ptcbp
 from PIL import Image, ImageOps
 from io import BytesIO
 
+def prepare_png(path, transform=True, padding=True, dither=True):
+    image = Image.open(path)
+    tmp = image.convert('1', dither=Image.FLOYDSTEINBERG if dither else Image.NONE)
+    tmp = ImageOps.invert(tmp.convert('L')).convert('1')
+    if transform:
+        tmp = tmp.rotate(-90, expand=True)
+        tmp = ImageOps.mirror(tmp)
+    if padding:
+        w, h = tmp.size
+        padded = Image.new('1', (128, h))
+        x, y = (128-w)//2, 0
+        nw, nh = x+w, y+h
+        padded.paste(tmp, (x, y, nw, nh))
+        tmp = padded
+    return tmp
+
 def encode_raster_transfer(data, nocomp=False):
     """ Encode 1 bit per pixel image data for transfer over serial to the printer """
     # Send in chunks of 1 line (128px @ 1bpp = 16 bytes)
@@ -22,17 +38,5 @@ def read_png(path, transform=True, padding=True, dither=True):
     This should work with any 8 bit PNG. To ensure compatibility, the image can
     be processed with Imagemagick first using the -monochrome flag.
     """
-    image = Image.open(path)
-    tmp = image.convert('1', dither=Image.FLOYDSTEINBERG if dither else Image.NONE)
-    tmp = ImageOps.invert(tmp.convert('L')).convert('1')
-    if transform:
-        tmp = tmp.rotate(-90, expand=True)
-        tmp = ImageOps.mirror(tmp)
-    if padding:
-        w, h = tmp.size
-        padded = Image.new('1', (128, h))
-        x, y = (128-w)//2, 0
-        nw, nh = x+w, y+h
-        padded.paste(tmp, (x, y, nw, nh))
-        tmp = padded
+    tmp = prepare_png(path, transform=transform, padding=padding, dither=dither)
     return tmp.tobytes()
